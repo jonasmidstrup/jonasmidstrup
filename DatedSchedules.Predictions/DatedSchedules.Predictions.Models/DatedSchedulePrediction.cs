6f9d751bd87c9f84372fee6f68df0cfd46ac78d3
Jonas Midstrup <jonas.midstrup@maersk.com>
2021-10-19 13:28:00 +0200

    all

---
+++ DatedSchedules.Predictions/DatedSchedules.Predictions.Models/DatedSchedulePrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace DatedSchedules.Predictions.Models
+{
+    public class DatedSchedulePrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedArrivalDifference { get; set; }
+    }
+}
